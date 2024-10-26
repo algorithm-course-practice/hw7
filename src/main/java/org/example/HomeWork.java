2db317943fcb1641ff4cc51f11294a7cc45efdb8
Alecos-Sad <alecos100@gmail.com>
2024-10-26 20:10:44 +0300

    hw7 solve

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,7 +1,10 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HomeWork {
 
@@ -11,7 +14,34 @@ public class HomeWork {
      * <a href="https://acm.timus.ru/problem.aspx?space=1&num=1439">https://acm.timus.ru/problem.aspx?space=1&num=1439</a>
      */
     public List<Integer> getOriginalDoorNumbers(int maxDoors, List<Action> actionList) {
-        return null;
+        Set<Integer> rooms = new HashSet<>();
+        List<Integer> results = new ArrayList<>();
+        int count;
+
+        for (int i = 0; i < Math.min(actionList.size(), maxDoors); i++) {
+            Action action = actionList.get(i);
+            int roomNumber = action.doorNumber;
+
+            if (!action.isLook) {
+                rooms.add(roomNumber);
+            } else {
+                count = 0;
+                for (int room : rooms) {
+                    if (room <= roomNumber) {
+                        count++;
+                    }
+                }
+
+                if (i == actionList.size() - 1) {
+                    count++;
+                }
+
+                int trueRoomNum = roomNumber + count;
+                results.add(trueRoomNum);
+            }
+        }
+
+        return results;
     }
 
     /**
@@ -28,7 +58,27 @@ public class HomeWork {
      * _ <b>4</b> => 4
      */
     public List<Integer> getLeaveOrder(int maxUnits, int leaveInterval) {
-        return null;
+        List<Integer> units = initializeUnits(maxUnits);
+        List<Integer> leaveOrder = new ArrayList<>();
+        int index = 0;
+
+        while (!units.isEmpty()) {
+            index = getNextIndex(index, leaveInterval, units.size());
+            leaveOrder.add(units.remove(index));
+        }
+
+        return leaveOrder;
     }
 
+    private List<Integer> initializeUnits(int maxUnits) {
+        List<Integer> units = new ArrayList<>();
+        for (int i = 1; i <= maxUnits; i++) {
+            units.add(i);
+        }
+        return units;
+    }
+
+    private int getNextIndex(int currentIndex, int interval, int size) {
+        return (currentIndex + interval - 1) % size;
+    }
 }
